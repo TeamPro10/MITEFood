a0836a5b31d411f23f43ef3e7cff17db500b150a
TeamPro10 <130374366+TeamPro10@users.noreply.github.com>
2024-07-17 13:43:43 +0530

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -23,11 +23,19 @@ document.getElementById("submit").addEventListener("click", (e) => {
     console.log("Checking email:", email);
     const exists = checkEmailExists(email);
 
+    if (/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=\[{\]}\\|;:'",<.>\/?]).{1,}$/.test(passw)) {
+        console.log("Valid password");
+    } else {
+        alert("Invalid Format: password should contain at least one uppercase letter, one lowercase letter, one number, and one special symbol.");
+        return;
+    }
+
     // After querying Firestore in checkEmailExists
     exists.then((exists) => {
         console.log("Email exists:", exists);
         if (!exists) {
             createUser(user, email, phone, passw);
+            
         } else {
             alert("This email already exists. Please use a different email.");
         }
@@ -53,7 +61,7 @@ async function createUser(user, email, phone, passw) {
         // Create the user in Firebase Authentication
         const userCredential = await auth.createUserWithEmailAndPassword(email, passw);
         const userId = userCredential.user.uid;
-        alert(phone)
+        // alert(phone)
         // Store additional user data in Firestore
         const usersRef = db.collection("Users");
         await usersRef.doc(email).set({
@@ -63,28 +71,11 @@ async function createUser(user, email, phone, passw) {
         });
         alert("Signup successful!");
         console.log("User created and data stored successfully.");
+        localStorage.setItem("inputValue", email);
+        window.location.href = `/index?username=${email}`;
     } catch (error) {
         console.error("Error creating user and storing data:", error);
         alert("This account already exists.");
     }
 }
 
-// document.getElementById("google-signin").addEventListener("click", async () => {
-//     try {
-//         const provider = new firebase.auth.GoogleAuthProvider();
-//         const result = await auth.signInWithPopup(provider);
-//         const user = result.user;
-        
-//         const exists = await checkEmailExists(user.email);
-
-//         if (!exists) {
-//             createUser(user.displayName, user.email, user.phone,"google-signin"); // You might not have access to phone and password from Google Sign-In
-//             // alert("Signup successful!");
-//         } else {
-//             alert("This account already exists.");
-//         }
-//     } catch (error) {
-//         console.error("Error signing in with Google:", error);
-//         alert("An error occurred. Please try again later.");
-//     }
-// });
